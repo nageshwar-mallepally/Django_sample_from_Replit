40762d19dae20c0e1ca90878366335d42077ca4c
nagesh028 <nageshreddy028@yahoo.com>
2021-12-21 19:34:45 +0530

    merge issues3

--- student/models.py
+++ student/models.py
@@ -5,9 +5,15 @@ class Student(models.Model):
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
   name = models.CharField(max_length=30)
   address = models.CharField(max_length=30)
   branch = models.CharField(max_length=15)
+=======
+  name = models.TextField()
+  address = models.TextField()
+  branch = models.TextField()
+>>>>>>> parent of d940ad9... Revert "Revert "Revert "Added Student module"""
 =======
   name = models.TextField()
   address = models.TextField()
