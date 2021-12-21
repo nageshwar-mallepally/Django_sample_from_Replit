f5b47d9e2f27ca0b4a29f8a154138dc4869f1bd1
nagesh028 <nageshreddy028@yahoo.com>
2021-12-21 19:24:32 +0530

    merge issues

--- student/models.py
+++ student/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
+<<<<<<< HEAD
   name = models.CharField(max_length=30)
   address = models.CharField(max_length=30)
   branch = models.CharField(max_length=15)
+=======
+  name = models.TextField()
+  address = models.TextField()
+  branch = models.TextField()
+>>>>>>> parent of d940ad9... Revert "Revert "Revert "Added Student module"""
---
+++ student/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from .models import Student
+
+
+# Create your views here.
+def students_view(request):
+  global first_time
+  all_students = Student.objects.all()
+  return render(request, 'index.html', 
+  {'all_items': all_students})
+
+def addStudent(request):
+  new_item = Student(content = request.POST['content'])
+  new_item.save()
+  return HttpResponseRedirect('/')
+
+def deleteStudent(request, s_id):
+  item_to_delete = Student.objects.get(id=s_id)
+  item_to_delete.delete()
+  return HttpResponseRedirect('/')
